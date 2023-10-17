191a69dcc350c5d6ddc0893c9c45d9c1b350c4f6
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2023-10-17 09:01:17 -0500

    Update conexion_internet.ino

--- conexion_internet.ino
+++ conexion_internet.ino
@@ -1,16 +1,16 @@
 #include "ESP8266WiFi.h"
-
+// Actualizar ssid, password y mqtt_server (ip del servidor MQTT) con los datos actuales de conexión del Laboratorio.
 const char* ssid = "Lab_ML340";
 const char* password = "UiSnfCra1";
 const char* mqtt_server = "157.253.205.17";
-
-void setup_wifi(){
+void setup_wifi() {
   delay(10);
+  // Inicio conexión red Wifi
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -20,11 +20,11 @@ void setup_wifi(){
   Serial.println(WiFi.localIP());
 }
 
-void setup(){
+void setup() {
   Serial.begin(115200);
   setup_wifi();
 }
 
-void loop(){
+void loop() {
   
 }
